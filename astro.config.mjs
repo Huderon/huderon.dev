d26fbd16a382c550fedeed28c2a029194e786f1b
Huderon <huderon.mcs@gmail.com>
2025-02-14 11:26:14 +1100

    test astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import {defineConfig} from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://huderon.dev",
+  site: "https://huderon.dev",
+  integrations: [svelte()],
 });
---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let messages = [];
+
+	let greeting = messages[0];
+
+	function randomMessage() {
+		return messages[Math.floor(Math.random() * messages.length)];
+	}
+</script>
+
+<div>
+	<h3>{greeting}! Thank you for visiting!</h3>
+	<button on:click={() => greeting = randomMessage()}>
+		New Greeting
+	</button>
+</div>
